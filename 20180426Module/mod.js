2effbc85dff338188ee8a79ae175173f55815e3b
xiao <892825058@qq.com>
2018-04-26 22:22:58 +0800

    提交练习代码

---
+++ 20180426Module/mod.js
@@ -0,0 +1,11 @@
+/*
+* require 请求 ：引入模块
+ module      模块
+ exports     输出
+ */
+// var a= 12;
+// exports.a=21;
+module.exports ={
+    a: 123,
+    b: 321
+}
---
+++ 20180426Module/timer.js
@@ -0,0 +1,5 @@
+setInterval(
+    () => {
+        console.log(Math.random())
+    },1000
+)
